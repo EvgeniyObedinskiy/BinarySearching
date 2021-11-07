6debb4c19b70c894b1a7d267e61983e0b76304f6
EVGENII OBEDINSKII <obe.work@gmail.com>
2021-11-07 08:52:53 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+       
+        public static string BinarySearch(int[] arr, int item)
+        {
+            ///Длинна поиска в массиве.
+            var arrLength = arr.Length;
+            ///Начало поиска в массиве
+            var arrStart = 0;
+            while (arrStart <= arrLength) 
+            {
+                ///Середина массива.
+                var arrMid = (arrStart + arrLength) / 2;
+                ///Значение элемента середины массива. 
+                var midItem = arr[arrMid];
+
+                if (midItem == item) 
+                {   ///Если зачение элемента середины массива равна искомому, возвращаем индекс.
+                    return arrMid.ToString();
+                }
+                if (midItem > item)
+                {   ///Если зачение элемента середины массива больше искомого, длинна поиска равна середине массива -1.
+                    arrLength = arrMid - 1;
+                }
+                else
+                    ///Иначе начало поиска в массиве равна середине массива +1.
+                    arrStart = arrMid + 1;
+            }
+            return "Not found";
+            
+        }
+        static void Main(string[] args) 
+        {
+            int[] list = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 123 };
+            string res = BinarySearch(list, 5);
+            Console.WriteLine(res);
+            Console.ReadLine();
+
+        }
+
+    }
+}
